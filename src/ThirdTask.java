470f4245cb9c9cbe163c628c4d5d5bc2d27b134c
Stonewall1 <impression97p@gmail.com>
2022-05-07 13:25:27 +0300

    ThirdTask

---
+++ src/ThirdTask.java
@@ -0,0 +1,11 @@
+public class ThirdTask {
+    public static void main(String[] args) {
+        double x = 5;
+        double y = 10;
+        double x2 = (Math.pow(x , 2));
+        double y2 = (Math.pow(y , 2));
+
+        double result = Math.sqrt(x2 + y2);
+        System.out.println(result);
+    }
+}
