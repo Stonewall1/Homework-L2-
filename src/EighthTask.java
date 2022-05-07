c92de99e51cd1934c4300f8234ae54c1c4753a12
Stonewall1 <impression97p@gmail.com>
2022-05-07 14:08:09 +0300

    EighthTask

---
+++ src/EighthTask.java
@@ -0,0 +1,8 @@
+public class EighthTask {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        int z = 2147483647 + 1;
+        System.out.println(z);
+    }
+    //Диапазон значений int от -2147483647 до + 2147483647. Поэтому добавляя 1 к максимальному значению, оно начинает считать заново от минимального
+}
